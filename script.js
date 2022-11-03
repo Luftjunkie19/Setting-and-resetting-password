9de74952e328eac8ab09ee43f2efafb43d14e8fe
Łukasz Borkowski <krawczyk0606@gmail.com>
2022-11-03 18:01:35 +0000

    Changes to make it more work + test user added

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ const changePasswordBtn = document.querySelector(".btn.change");
 const loginToReset = document.querySelector("#reset-login");
 const resetPassword = document.querySelector("#reset-password");
 const resetAgainPassword = document.querySelector("#reset-again");
+const comeBackToLoginBtn = document.querySelector(".btn.come-back-log");
 //Final message
 const finalMessage = document.querySelector(".final-msg");
 const finalTitle = document.querySelector(".final-title");
@@ -40,6 +41,9 @@ class User {
     registrated.push(user);
   }
 }
+
+const testUser = new User("Kacper", "Kacperito@gmail.com", "Dzik_JTCNW100%");
+registrated.push(testUser);
 
 function removeAllClasses(element) {
   element.classList = ``;
@@ -65,7 +69,7 @@ function showPassword(e) {
   if (e.target.classList.contains("eye-on")) {
     e.target.style.display = `none`;
     //
-    e.target.nextElementSibling.style.display = "block";
+    e.target.nextElementSibling.style.display = "flex";
     //
     e.target.previousElementSibling.type = "text";
   }
@@ -75,7 +79,7 @@ function vanishPassword(e) {
   if (e.target.classList.contains("eye-off")) {
     e.target.style.display = `none`;
     //
-    e.target.previousElementSibling.style.display = "block";
+    e.target.previousElementSibling.style.display = "flex";
     //
     e.target.previousElementSibling.previousElementSibling.type = "password";
   }
@@ -163,18 +167,18 @@ regSubmitBtn.addEventListener("click", () => {
     console.log(registrated);
 
     registerContainer.style.display = `none`;
-    loggingContainer.style.display = `block`;
+    loggingContainer.style.display = `flex`;
   }
 });
 
 haveAccountBtn.addEventListener("click", () => {
   registerContainer.style.display = `none`;
-  loggingContainer.style.display = `block`;
+  loggingContainer.style.display = `flex`;
 });
 
 function showFinalMessage(username) {
   loggingContainer.style.display = `none`;
-  finalMessage.style.display = `block`;
+  finalMessage.style.display = `flex`;
   finalTitle.innerText = `Thank you, ${username}`;
   finalText.innerText = `For opening and trying my project, I appreciate it. If you want, you
   can do it again😁`;
@@ -185,19 +189,37 @@ function loggingIn() {
   let enteredPassword = passwordLogin.value;
 
   registrated.forEach((user) => {
-    if (user.nickname === enteredLogin && enteredPassword === user.password) {
+    if (
+      enteredLogin === user.nickname ||
+      (enteredLogin === user.email && enteredPassword === user.password)
+    ) {
       showFinalMessage(user.nickname);
+      removeAllClasses(login);
+      removeAllClasses(passwordLogin);
     } else {
-      console.log("NO");
+      if (enteredLogin !== user.nickname) {
+        removeAllClasses(login);
+        showWrong(login, "Check your login, it might be wrong");
+      } else if (enteredPassword !== user.password) {
+        removeAllClasses(passwordLogin);
+        showWrong(passwordLogin, "Password is wrong, maybe change it?");
+      }
     }
   });
 }
 
 function openResetForm() {
   loggingContainer.style.display = `none`;
-  resetPasswordContainer.style.display = "block";
+  resetPasswordContainer.style.display = "flex";
 }
 resetPasswordBtn.addEventListener("click", openResetForm);
+
+function backToLogin() {
+  resetPasswordContainer.style.display = `none`;
+  loggingContainer.style.display = `flex`;
+}
+
+comeBackToLoginBtn.addEventListener("click", backToLogin);
 
 loginSubmit.addEventListener("click", loggingIn);
 againBtn.addEventListener("click", () => {
@@ -217,10 +239,18 @@ function getUser() {
 
       registrated.splice(i, 1, user);
 
-      loggingContainer.style.display = `block`;
+      loggingContainer.style.display = `flex`;
       resetPasswordContainer.style.display = "none";
+
+      loginToReset.value = ``;
+      resetPassword.value = ``;
+      resetAgainPassword.value = ``;
+
+      removeAllClasses(resetPassword);
+      removeAllClasses(resetAgainPassword);
     } else {
       console.log("NO");
+      console.log(registrated);
     }
   });
 }
